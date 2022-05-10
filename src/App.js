fc414157ecff4cb9e9d8981ca498fe24f5d2c50c
will <denis.barbosa@fatec.sp.gov.br>
2022-05-10 10:48:36 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { BrowserRouter as Router } from 'react-router-dom'
+import { Routes } from './routes'
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <Router>
+      <Routes/>
+
+    </Router>
+  );
+}
+
+export default App;
